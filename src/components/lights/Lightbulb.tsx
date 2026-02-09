ff8a8d5344db9c51af5d2829d42945407e6e9cae
Thiago Trotta <trottathiago1@gmail.com>
2026-02-09 05:58:30 -0300

    feat: remodel 3D lightbulb model

---
+++ src/components/lights/Lightbulb.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { Group } from "three";
+import { useFloatingAnimation } from "@/hooks/useFloatingAnimation";
+import { useScrollRotation } from "@/hooks/useScrollAnimation";
+import { useLightAnimation } from "@/hooks/useLightAnimation";
+import { useResponsiveScale } from "@/hooks/useResponsiveScale";
+import LightWrapper from "./LightWrapper";
+import { lightResponsiveConfig } from "@/configs/lightResponsiveConfig";
+
+export default function Lightbulb() {
+  const staticGroupRef = useRef<Group>(null);
+  const animatedGroupRef = useRef<Group>(null);
+
+  const { scene } = useGLTF("/models/lightbulb.glb");
+
+  const { lbScale, sphereRadius, lbPosition, spherePosition } =
+    useResponsiveScale(
+      lightResponsiveConfig.mobile,
+      lightResponsiveConfig.desktop,
+    );
+
+  useFloatingAnimation(animatedGroupRef);
+  useScrollRotation(animatedGroupRef);
+
+  const { pointLightRef, sphereMeshRef, glowRef } = useLightAnimation(true);
+
+  return (
+    <group ref={staticGroupRef} position={lbPosition}>
+      <group ref={animatedGroupRef}>
+        <pointLight
+          ref={pointLightRef}
+          position={spherePosition}
+          color="#fef254"
+          intensity={2}
+          distance={10}
+          decay={2}
+        />
+
+        <LightWrapper
+          spherePosition={spherePosition}
+          sphereRadius={sphereRadius}
+          sphereMeshRef={sphereMeshRef}
+          glowRef={glowRef}
+        />
+
+        <primitive object={scene} scale={lbScale} rotation={[0, 0, 0]} />
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/models/lightbulb.glb");
