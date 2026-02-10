d3014dee1d5664e6688f9cd1be4a14198831a306
Thiago Trotta <trottathiago1@gmail.com>
2026-02-10 20:14:53 -0300

    feat: add SplitText plugin to use in AboutMe section

--- src/components/ui/AboutMe.tsx
+++ src/components/ui/AboutMe.tsx
@@ -1,11 +1,46 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import SplitText from "gsap/src/SplitText";
+import { useRef } from "react";
+
 export default function AboutMe() {
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      const tl = SplitText.create(".split p", {
+        type: "lines, words",
+        linesClass: "overflow-hidden",
+      });
+
+      gsap.from(tl.lines, {
+        yPercent: 100,
+        opacity: 0,
+        duration: 1.2,
+        stagger: 0.03,
+        ease: "power4.out",
+        scrollTrigger: {
+          trigger: ".split",
+          start: "top 50%",
+          toggleActions: "play none none reverse",
+        },
+      });
+    },
+    { scope: container },
+  );
+
   return (
-    <section className="relative z-10 min-h-screen w-full border-b border-neutral-800 py-20">
+    <section
+      ref={container}
+      className="relative z-10 min-h-screen w-full py-20"
+    >
       <div className="container mx-auto px-4 md:px-10">
         <h2 className="font-heading mb-10 text-6xl font-bold text-white">
           Who am I?
         </h2>
-        <div className="flex max-w-3xl flex-col gap-6 text-lg leading-relaxed text-neutral-300 md:text-xl">
+        <div className="split max-w-3xl space-y-6 text-lg leading-relaxed text-neutral-300 md:text-xl">
           <p>
             My name is Thiago Trotta and I&apos;m from Argentina. I got into
             programming and the IT world when I was 16. That&apos;s why I&apos;m
