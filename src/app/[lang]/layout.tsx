ed47b44a7843877a0fb4406d6a737362992b0b4f
Thiago Trotta <trottathiago1@gmail.com>
2026-02-27 04:34:37 -0300

    style: remove unused fonts

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,12 +1,5 @@
 import "../globals.css";
-import {
-  inter,
-  playfairDisplay,
-  dmSans,
-  satisfy,
-  spaceGrotesk,
-  michroma,
-} from "../../lib/fonts";
+import { inter, dmSans, michroma } from "../../lib/fonts";
 import type { Metadata, Viewport } from "next";
 import SmoothScroll from "@/components/SmoothScroll";
 import { CustomScroll } from "@/components/CustomScroll";
@@ -42,7 +35,7 @@ export default async function RootLayout({
   return (
     <html
       lang={lang}
-      className={`${inter.variable} ${playfairDisplay.variable} ${dmSans.variable} ${satisfy.variable} ${spaceGrotesk.variable} ${michroma.variable}`}
+      className={`${inter.variable} ${dmSans.variable} ${michroma.variable}`}
     >
       <body className={"bg-background font-body text-texting antialiased"}>
         <Navbar lang={lang} dict={dict.navbar} />
--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,18 +1,4 @@
-import {
-  Playfair_Display,
-  Inter,
-  DM_Sans,
-  Satisfy,
-  Space_Grotesk,
-  Michroma,
-} from "next/font/google";
-
-export const playfairDisplay = Playfair_Display({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  display: "swap",
-  variable: "--font-playfair-display",
-});
+import { Inter, DM_Sans, Michroma } from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
@@ -25,19 +11,6 @@ export const dmSans = DM_Sans({
   weight: ["400", "500", "700"],
   display: "swap",
   variable: "--font-dm-sans",
-});
-
-export const satisfy = Satisfy({
-  subsets: ["latin"],
-  weight: "400",
-  display: "swap",
-  variable: "--font-satisfy",
-});
-
-export const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-space-grotesk",
 });
 
 export const michroma = Michroma({
